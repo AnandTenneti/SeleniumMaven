094285804414c645d33a58b97c26dadd0a7623ef
AnnadTenneti <tenneti.anand@gmail.com>
2024-08-12 09:22:17 +0530

    Added seleniumjenkins file

---
+++ src/test/java/mavenforjenkins/UiTest.java
@@ -0,0 +1,20 @@
+package mavenforjenkins;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UiTest {
+
+    @Test
+    public void openApplication() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"));
+        driver.quit();
+
+    }
+}
