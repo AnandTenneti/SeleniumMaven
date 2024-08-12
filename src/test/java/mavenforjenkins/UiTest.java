90b775757691290f1d273787307ad5b0d0793645
AnnadTenneti <tenneti.anand@gmail.com>
2024-08-12 17:26:14 +0530

    Parameter added

--- src/test/java/mavenforjenkins/UiTest.java
+++ src/test/java/mavenforjenkins/UiTest.java
@@ -3,26 +3,39 @@ package mavenforjenkins;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class UiTest {
+    WebDriver driver = null;
+
+
+    @Parameters("Browser")
+    @BeforeMethod
+    public void browserSetUp(String browserName){
+        System.out.println("Parameter value is " + browserName);
+        if (browserName.contains("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if(browserName.contains("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+    }
 
     @Test
     public void openApplication() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+            driver.get("https://www.google.com");
         Assert.assertTrue(driver.getTitle().contains("Google"));
         driver.quit();
-
     }
-  @Test
+    @Test
     public void openNewApplication() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        Assert.assertTrue(driver.getTitle().contains("Google"));
+        driver.get("https://www.facebook.com");
+        Assert.assertTrue(driver.getTitle().contains("Facebook"));
         driver.quit();
     }
 }
