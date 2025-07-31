507bbaaa57cc2cb0e76e25d2d51ed0194447a81a
Anand Kiran Tenneti <159147328+AnandTenneti@users.noreply.github.com>
2025-07-31 22:42:22 +0530

    Update UiTest.java

--- src/test/java/mavenforjenkins/UiTest.java
+++ src/test/java/mavenforjenkins/UiTest.java
@@ -17,7 +17,7 @@ public class UiTest {
    
     @BeforeMethod
     public void browserSetUp() {
-        browserName="chrome";
+        String browserName="chrome";
         System.out.println("Parameter value is " + browserName);
         if (browserName.contains("chrome")) {
             WebDriverManager.chromedriver().setup();
